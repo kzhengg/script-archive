2a5224224c4d4720487a8c429adfa58bd6f5c042
Kevin Zheng <wenli2003@icloud.com>
2023-12-25 21:43:21 -0500

    backend stuff in progress

---
+++ server/server.js
@@ -0,0 +1,40 @@
+import express from 'express';
+import cors from 'cors'
+import { connect } from "./db.js";
+import { Snippet } from "./snippetmodel.js";
+import * as randomstring from "randomstring";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware to parse JSON bodies
+app.use(express.json());
+app.use(cors())
+
+// Handle POST request on /api/snippets
+app.post('/api/snippets', async (req, res) => {
+  await connect();
+
+  if (req.method === "POST") {
+    const slug = randomstring.generate({
+      length: 6,
+      charset: "alphabetic",
+    });
+    const snippet = await Snippet.create({
+      snippet: req.body.snippet,
+      slug,
+    });
+
+    res.statusCode = 200;
+    res.json(snippet);
+  } else {
+    throw new Error(
+      "http method not supported on this endpoint"
+    );
+  }
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
--- src/components/CreateScript.vue
+++ src/components/CreateScript.vue
@@ -1,4 +1,28 @@
 <script setup lang="ts">
+import { ref } from 'vue';
+
+const snippetContent = ref('');
+
+const createSnippet = async () => {
+  try {
+    const response = await fetch('http://localhost:3000/api/snippets', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ content: snippetContent.value }),
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    // Handle the response here (e.g., show a success message)
+  } catch (error) {
+    console.error('Error creating snippet:', error);
+    // Handle the error here (e.g., show an error message)
+  }
+};
 </script>
 
 <template>
@@ -6,27 +30,16 @@
     <h1 class="text-4xl font-bold text-gray-800 mb-2">Upload your snippet here!</h1>
     <p class="text-lg text-gray-600 mb-4">Paste any script below, save and share with friends</p>
     <div class="flex flex-col space-y-4 p-4">
-    <!-- Textbox for Writing Scripts -->
-    <textarea 
+      <textarea 
+        v-model="snippetContent"
         class="w-full h-64 p-4 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
         placeholder="Write your script here...">
-    </textarea>
-
-    <!-- File Upload Input -->
-    <div class="flex items-center justify-center w-full">
-        <label class="flex flex-col items-center px-4 py-2 bg-white text-blue rounded-lg shadow-lg tracking-wide uppercase border border-blue cursor-pointer hover:bg-blue-500 hover:text-white">
-            <svg class="w-8 h-8" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                <path d="M16.7,5.3,10.7.3a1,1,0,0,0-1.4,0L3.3,5.3A1,1,0,0,0,3,6V17a1,1,0,0,0,1,1H16a1,1,0,0,0,1-1V6A1,1,0,0,0,16.7,5.3ZM12,16H8V12h4v4Zm1-5H7V6l5-4,5,4v5Z"/>
-            </svg>
-            <span class="mt-2 text-base leading-normal">Select a file</span>
-            <input type='file' class="hidden" />
-        </label>
+      </textarea>
+      <!-- Rest of your code for file upload input -->
     </div>
-</div>
-
+    <button @click="createSnippet" class="w-48 px-6 py-3 bg-green-500 text-white rounded-md shadow hover:bg-green-600">Create Snippet</button>
   </div>
 </template>
-
 
 <style scoped>
 </style>
